43c2d0845a4d557e4bb123089c897da4d5d72103
kimgsun <gsune@kakao.com>
2025-08-12 16:25:41 +0900

    20250812 Spring Boot

---
+++ src/main/java/com/example/k5_iot_springboot/service/B_StudentService.java
@@ -0,0 +1,22 @@
+package com.example.k5_iot_springboot.service;
+
+import com.example.k5_iot_springboot.dto.B_Student.StudentCreateRequestDto;
+import com.example.k5_iot_springboot.dto.B_Student.StudentResponseDto;
+import com.example.k5_iot_springboot.dto.B_Student.StudentUpdateRequestDto;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface B_StudentService {
+    StudentResponseDto createStudent(StudentCreateRequestDto requestDto);
+
+    List<StudentResponseDto> getAllStudents();
+
+    StudentResponseDto getStudentById(Long id);
+
+    StudentResponseDto updateStudent(Long id, StudentUpdateRequestDto requestDto);
+
+    void deleteStudent(Long id);
+
+    List<StudentResponseDto> filterStudentsByName(String name);
+}
