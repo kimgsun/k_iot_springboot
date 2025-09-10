4f863e21fed5cfb4b281c3629e2b6876ef9d4a9e
kimgsun <gsune@kakao.com>
2025-09-10 16:25:27 +0900

    20250910 USER/USER_ROLE/ROLE

--- src/main/java/com/example/k5_iot_springboot/service/impl/G_AuthServiceImpl.java
+++ src/main/java/com/example/k5_iot_springboot/service/impl/G_AuthServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.k5_iot_springboot.service.impl;
 
+import com.example.k5_iot_springboot.common.enums.RoleType;
 import com.example.k5_iot_springboot.dto.G_Auth.request.SignInRequest;
 import com.example.k5_iot_springboot.dto.G_Auth.request.SignUpRequest;
 import com.example.k5_iot_springboot.dto.G_Auth.response.SignInResponse;
 import com.example.k5_iot_springboot.dto.ResponseDto;
+import com.example.k5_iot_springboot.entity.G_Role;
 import com.example.k5_iot_springboot.entity.G_User;
 import com.example.k5_iot_springboot.provider.JwtProvider;
+import com.example.k5_iot_springboot.repository.G_RoleRepository;
 import com.example.k5_iot_springboot.repository.G_UserRepository;
 import com.example.k5_iot_springboot.service.G_AuthService;
 import io.jsonwebtoken.Claims;
@@ -31,6 +34,7 @@ public class G_AuthServiceImpl implements G_AuthService {
     //      , 스프링 컨테이너에 등록될 때 PasswordEncoder 타입으로 인식 (주입 시 해당 타입으로 정의 권장)
     private final AuthenticationManager authenticationManager;
     private final JwtProvider jwtProvider;
+    private final G_RoleRepository roleRepository;
 
     @Override
     @Transactional
@@ -57,6 +61,11 @@ public class G_AuthServiceImpl implements G_AuthService {
                 .nickname(req.nickname())
                 // .gender(req.gender()) - null 허용
                 .build();
+
+        // 기본 권한 부여
+        // cf) getReferenceById: 특정 ID를 가진 엔티티의 프록시 객체를 즉시 반환
+        G_Role defaultRole = roleRepository.getReferenceById(RoleType.USER);
+        user.grantRole(defaultRole); // 변경 감지로 user_roles가 insert 됨 (cascade=ALL)
 
         userRepository.save(user);
     }
